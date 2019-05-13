f50e8c9310bef8715513636daaad0df187bd78d4
JLO <joshualopesm@gmail.com>
2019-05-12 18:06:54 -0600

    master: changed to css, no need for scss
    New landing page background
    Started work on navigation bar

---
+++ src/navigation/NavigationBar.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class NavigationBar extends React.PureComponent{
+
+    navigateTo = () =>{
+        switch(page){
+            case 'home':
+                //todo nav to home
+                break;
+            case 'tech':
+                //todo nav to tech
+                break;
+            case 'work':
+                //todo nav to work
+                break;
+            case 'resume':
+                //todo nav to resume
+                break;
+            default:
+                // why you trying to break me ):
+                break;
+        }
+    }
+    render(){
+        return (
+        <div class="topnav">
+            <div onClick>Home</div>
+            <div>Tech Experience</div>
+            <div>Work Experience</div>
+            <div>Resume</div>
+        </div>
+        );
+    }
+}
+
+export default NavigationBar;
--- src/navigation/front-page/LandingPage.jsx
+++ src/navigation/front-page/LandingPage.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import image from '../../static/images/LandingPageImage00.JPG';
+import './LandingPage.css';
 
 class LandingPage extends React.PureComponent {
   render() {
-    return <div>Landing Page</div>;
+    return (
+      <div className="landing-page">
+        <div className="landing-header">
+          <h1>Joshua Lopes Website</h1>
+        </div>
+        <div className="overlay" />
+        <img className="landing-image" src={image} alt="error" />
+      </div>
+    );
   }
 }
 
