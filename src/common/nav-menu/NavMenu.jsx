4e4adb6672ae51ee9045850ef6664fe287bd8c79
JLO <joshualopesm@gmail.com>
2019-05-27 21:16:01 -0600

    master: implemented drop down
    Material UI being used now
    Nav Menu is using Material UI

---
+++ src/common/nav-menu/NavMenu.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import "./NavMenu.css";
+
+function NavMenu() {
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    function handleClick(event) {
+        setAnchorEl(event.currentTarget);
+    }
+
+    function handleClose() {
+        setAnchorEl(null);
+    }
+
+    return (
+        <div className="nav-menu">
+            <Button
+                aria-owns={anchorEl ? 'simple-menu' : undefined}
+                aria-haspopup="true"
+                onClick={handleClick}
+            >
+                Navigation
+            </Button>
+            <Menu id="nav-menu" className="hover-override" anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleClose}>
+                <MenuItem onClick={handleClose}>Home</MenuItem>
+                <MenuItem onClick={handleClose}>Resume</MenuItem>
+                <MenuItem onClick={handleClose}>Work Experience</MenuItem>
+                <MenuItem onClick={handleClose}>Tech Experience</MenuItem>
+            </Menu>
+        </div>
+    );
+}
+
+export default NavMenu;
--- src/navigation/NavigationBar.jsx
+++ src/navigation/NavigationBar.jsx
@@ -1,37 +1,18 @@
 import React from 'react';
 import './NavigationBar.css';
+import NavMenu from '../common/nav-menu/NavMenu';
 
-class NavigationBar extends React.PureComponent {
+class NavigationBar extends React.Component {
     render() {
         return (
             <div className="top-nav">
-                <div className="left-nav" onClick={() => NavigateTo('home')}> Joshua Lopes </div>
-                <div className="right-nav" onClick={() => NavigateTo('resume')}>Resume</div>
-                <div className="right-nav" onClick={() => NavigateTo('work')}>Work Experience</div>
+                <div className="left-nav" onClick={() => this.avigateTo('home')}> Joshua Lopes </div>
+                <NavMenu />
             </div>
         );
     }
 }
 
-function NavigateTo(page) {
-    switch (page) {
-        case 'home':
-            console.log('hello home');
-            //todo nav to home
-            break;
-        case 'tech':
-            //todo nav to tech
-            break;
-        case 'work':
-            //todo nav to work
-            break;
-        case 'resume':
-            //todo nav to resume
-            break;
-        default:
-            // why you trying to break me ):
-            break;
-    }
-}
+
 
 export default NavigationBar;
