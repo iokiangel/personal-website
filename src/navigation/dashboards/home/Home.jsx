c76ce7f220aa56addbce79fb094867b94a919af1
iokiangel <joshualopesm@gmail.com>
2019-07-17 20:23:09 -0600

    master - hotfix

--- src/navigation/dashboards/home/Home.jsx
+++ src/navigation/dashboards/home/Home.jsx
@@ -11,7 +11,7 @@ export default class Home extends React.Component {
                 <div className="description">
                     Hello, my name is Joshua and I've created this website as an interactive Resume to show my Full Stack Developer experience.
                     This website was written using a Javascript library called React.
-                    My application is being hosted using AWS CloudFront and an S3 bucket as an origin.
+                    My application is being hosted using AWS.
                     I also have an AWS CI/CD pipeline hooked up to my repo which you can check out at my Github.
                     <a href="https://github.com/iokiangel/personal-website" target="_blank">
                         <img className="github-icon" src={githubIcon} alt="error" />
